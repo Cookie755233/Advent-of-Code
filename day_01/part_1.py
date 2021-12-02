d180da499a917874cfc6b2f437dd4e42644a218f
Cookie <cookie010103@gmail.com>
2021-12-02 17:10:01 +0800

    ''

--- day_01/part_1.py
+++
@@ -1,7 +0,0 @@
-with open('./data.in') as f:
-    s = 0
-    l = [int(i) for i in f.readlines()]
-    for i in range(len(l)-1):
-        if l[i]<l[i+1]: s +=1
-    
-    print(s)
--- day_01/part_2.py
+++
@@ -1,7 +0,0 @@
-with open('./data.in') as f:
-    s = 0
-    l = [int(i) for i in f.readlines()]
-    for i in range(len(l)-3):
-        if sum(l[i:i+3])<sum(l[i+1:i+4]): s +=1
-    
-    print(s)
